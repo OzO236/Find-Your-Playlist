6553dd1fa4303aa829ccbd637403490282adcf5d
yeonju9623 <yeonju9623@naver.com>
2024-04-09 17:46:05 +0900

    yj: App.css 초기화

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './Fonts/Font.css';
 import { Route, Routes } from 'react-router-dom';
+import Main from './components/Main.js';
+import Playlist from './components/Playlist.js';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<div>메인페이지</div>}></Route>
+        <Route path='/' element={<Main/>}></Route>
         <Route path='/question' element={<div>질문페이지</div>}></Route>
         <Route path='/result' element={<div>결과페이지</div>}></Route>
       </Routes>
---
+++ src/components/Main.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from '../styles/Main.module.css';
+import clt from '../asset/main_playctl.png';
+
+const Main = () => {
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.screen}>
+        <div className={styles.chats}>
+          <div className={styles.chat1}>
+            <p>당신의 K-POP 플리를 찾아드림</p>
+          </div>
+          <div className={styles.chat2}>
+            <p>나에게 맞는 K-POP 플리 찾으러 가기</p>
+          </div>
+        </div>
+        <div className={styles.visited}>
+          <p>방문자 수: 500</p>
+        </div>
+      </div>
+
+      <div className={styles.playctl}>
+        <img src={clt} alt="no clt img" />
+      </div>
+
+    </div>
+  )
+}
+
+export default Main
